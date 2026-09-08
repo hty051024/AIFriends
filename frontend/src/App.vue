42e97dae6c4d40246dc23245ecd4c72caec7ca06
ty h <27895824@qq.com>
2026-09-09 01:06:42 +0800

    实现了路由

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,8 @@ import NavBar from "@/components/navbar/NavBar.vue";
 
 <template>
   <NavBar>
-
+    <RouterView />
+<!--                                //RouterView作用：自动根据后面的url去index.js里找这个url对应的component，并把其渲染到RouterView这个位置-->
   </NavBar>
 
 
---
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {useRoute, useRouter} from "vue-router";
+
+const route=useRoute()           //取当前url相关信息
+const router=useRouter()         //操作打开一个界面或后退，例子如下
+//
+// router.push({
+//   name:'homepage-index'
+// })                             实现功能：打开用户信息界面自动跳转首页
+</script>
+
+<template>
+个人空间:{{ route.params.user_id }}
+</template>
+
+<style scoped>
+
+</style>
