96b6fb08fccaa654514c7fb64598d0e28a59cba5
ty h <27895824@qq.com>
2026-09-09 02:23:45 +0800

    实现了数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)    #逗号一定不能删除！！  因为这里要传的是列表，如果去掉后就是一个元素了
